96f5b20b68fb6ed68756eaa5b492c28e88fd473e
Adnan Rashid <58908269+addu2146@users.noreply.github.com>
2025-04-25 22:56:55 +0530

    Update Caeser.py

--- adduciphers/Caeser.py
+++ adduciphers/Caeser.py
@@ -1,1 +1,16 @@
-
+def Caeser(plaintext: str,key: int ,mode:str)->str:
+    result = []
+    shift =key if mode == "encrypt" else -key
+    for c in plaintext:
+        if 'A'<=c<='Z':
+            result.append(chr((ord(c)-65+shift)%26+65))
+        elif 'a'<=c<='z':
+            result.append(chr((ord(c)-97+shift)%26+97))
+        else: result.append(c)
+    return ''.join(result)
+plaintext = 'Micheal'
+key = 2
+enc = Caeser(plaintext,key,"encrypt")
+dec = Caeser(enc,key,"decrypt")
+print("encrypted: ",enc)
+print("Decrypted: ",dec)
